7b21cee6a36046a3802e0be919556568b726b225
Joao-Salvalagio <joaosalvalagio@gmail.com>
2026-04-01 18:06:02 -0300

    Terceiro exercicio feito, ainda estou oscilando bastante, mas teve uma melhora boa, acredito que mais 2 exercicios ja pego a estrutura completa.

--- Treino - Prova de Flutter/flutter_treino/lib/main.dart
+++ Treino - Prova de Flutter/flutter_treino/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_treino/exc1_pecas.dart';
 import 'package:flutter_treino/exc2_alunos.dart';
+import 'package:flutter_treino/exc3_livros.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: AlunoScreen(),
+      home: LivroScreen(),
     );
   }
 }
